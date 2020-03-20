71a9b29948091fcc6a0e688e6462970bb3964579
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-20 14:20:36 -0700

    Improve experience content
    - Add 1.2M requests per month metric
    - Add CI/CD pipeline for testing suites

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,17 +5,17 @@ export default {
       company: { name: "Make School", url: "https://makeschool.com" },
       startDate: new Date("2018-10-22"),
       accomplishments: [
-        "Setup benchmark, accessibility and integration testing suites using Google Lighthouse, Axe and Cypress.",
-        "Create accessible and reusable React/Apollo components and helpers for forms (including validation), tables, data fetching and error handling.",
-        "Implement a style guide and setup JavaScript linting/prettify/typing configuration to enforce it throughout the entire codebase. Helped reduce technical debt ratio by 50% (from ~2% to ~1%), while adding 2X more features (from ~28K to ~70K lines of JavaScript code) over 18 months.",
+        "Setup performance, accessibility and integration testing suites for CI/CD pipeline using Google Lighthouse, Axe and Cypress.",
         "Increase number of React unit tests with Jest/Enzyme by more than 4X (from ~200 to ~900 specs) within one year.",
+        "Implement a style guide and setup JavaScript linting/prettify/typing configuration to enforce it throughout the entire codebase. Helped reduce technical debt ratio by 50% (from ~2% to ~1%), while adding 2X more features (from ~28K to ~70K lines of JavaScript code) over 18 months.",
+        "Create accessible and reusable React/Apollo components and helpers for forms (including validation), tables, data fetching and error handling.",
         "Lead peer workshops on unit/integration testing and GraphQL contracts, and upgrade/migrate the entire frontend from Relay Classic/React v15 -> Apollo/React v16."
       ],
       responsibilities: [
-        "Build/maintain frontend and backend tools like course registrations, attendance tracker, and an advanced query builder table to support staff and students (Ruby on Rails, React, GraphQL, Apollo).",
+        "Build/maintain frontend and backend tools like course registrations, attendance tracker, and an advanced query builder table to support staff and students at scale of ~1.2M requests per month (Ruby on Rails, React, GraphQL, Apollo).",
+        "Collaborate closely with design to create accessible, mobile-friendly pages and a detailed style guide.",
         "Coach group of five students on writing production-ready querying/mutating data using GraphQL and writing composable React components for a student project on our company's codebase.",
-        "Conduct in-house user testing with staff and students (main users) to improve existing features and inform development of new features.",
-        "Collaborate closely with design to create accessible, mobile-friendly pages and a detailed style guide."
+        "Conduct in-house user testing with staff and students (main users) to improve existing features and inform development of new features."
       ]
     },
     {
@@ -29,9 +29,9 @@ export default {
         'Develop strong partnerships with executives at Apple, Facebook, and several Oscar-winning directors. Resulted in several feature-length films and music videos created on an iPhone with Beastgrip gear. (Steven Soderberg\'s "Unsane", "Olivia Wilde\'s "No Love Like Yours", and John Legend\'s "A Good Night")'
       ],
       responsibilities: [
-        "Build close relationships with mobile filmmakers, journalists and media companies through trade shows, filmmaking projects and cold calls/emails.",
         "Assist in design of new rigs, lenses and applications (mobile and web) with direct communication from our users and internal dogfooding/film projects.",
-        "Write, record video/photo content and host live streams for blog, social media, and product descriptions."
+        "Write, record video/photo content and host live streams for blog, social media, and product descriptions.",
+        "Build close relationships with mobile filmmakers, journalists and media companies through trade shows, filmmaking projects and cold calls/emails."
       ]
     }
   ]
