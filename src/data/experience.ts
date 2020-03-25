ba1623301dd6781f5ebbef37b1150d95362bf1ac
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-25 00:10:19 -0700

    Tiny upgrades too experience data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,10 +9,10 @@ export default {
         "Increase number of React unit tests with Jest/Enzyme by more than 4X (from ~200 to ~900 specs) within one year.",
         "Implement a style guide and setup JavaScript linting/prettify/typing configuration to enforce it throughout the entire codebase. Helped reduce technical debt ratio by 50% (from ~2% to ~1%), while adding 2X more features (from ~28K to ~70K lines of JavaScript code) over 18 months.",
         "Create accessible and reusable React/Apollo components and helpers for forms (including validation), tables, data fetching and error handling.",
-        "Lead peer workshops on unit/integration testing and GraphQL contracts, and upgrade/migrate the entire frontend from Relay Classic/React v15 -> Apollo/React v16."
+        "Lead peer workshops on unit/integration testing and GraphQL contracts, and upgrade/migrate the entire frontend of ~40K lines of code from Relay Classic/React v15 -> Apollo Client/React v16."
       ],
       responsibilities: [
-        "Build/maintain frontend and backend tools like course registrations, attendance tracker, and an advanced query builder table to support staff and students at scale of ~1.2M requests per month (Ruby on Rails, React, GraphQL, Apollo).",
+        "Build/maintain frontend and backend tools like course registrations, attendance tracker, and an advanced query builder table to support staff and students at scale of ~1.2M requests per month (Ruby on Rails, React, GraphQL, Apollo Client).",
         "Collaborate closely with designer to create accessible, mobile-friendly pages and a detailed style guide.",
         "Coach group of five students on writing production-ready querying/mutating data using GraphQL and writing composable React components for a student project on our company's codebase.",
         "Conduct in-house user testing with staff and students (main users) to improve existing features and inform development of new features."
@@ -23,7 +23,7 @@ export default {
       company: { name: "Beastgrip", url: "https://beastgrip.com" },
       startDate: new Date("2015-01-10"),
       accomplishments: [
-        "Lead development of a custom-built e-commerce platform with user permissions, CMS and integrated shipping service (Node, Express, React).",
+        "Lead development of a custom-built e-commerce platform with user permissions, CMS and integrated shipping service (React, Node/Express).",
         "Support designer on user interface and experience for an advanced mobile camera app (Beastcam) that includes manual controls, digital slate and an internal gallery.",
         "Assist in raising over 400K over several Kickstarter campaigns with copywriting, video and social media content creation and Facebook advertisements.",
         'Develop strong partnerships with executives at Apple, Facebook, and several Oscar-winning directors. Resulted in several feature-length films and music videos created on an iPhone with Beastgrip gear. (Steven Soderberg\'s "Unsane", "Olivia Wilde\'s "No Love Like Yours", and John Legend\'s "A Good Night")'
