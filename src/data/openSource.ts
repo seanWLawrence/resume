0703c59eab171850a921cb531c5035895ebb0469
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-26 00:52:16 -0700

    Improve coopy for oopen source and public speaking sectioons
    - add details about what/why the packages and talks weree given and what they
      accomplished

--- src/data/openSource.ts
+++ src/data/openSource.ts
@@ -2,23 +2,24 @@ export default {
   openSourceContributions: [
     {
       name: "webpacker",
-      url: "https://github.com/rails/webpacker",
+      url: "https://github.com/rails/webpacker/issues/1897",
       programmingLanguage: "Ruby",
       description:
-        "Add documentation for minification with existing Sprockets asset pipeline."
+        "Added documentation for troubleshooting an error when using Webpacker with an existing Sprockets asset pipeline. Without this confirguration setting, the uglifier minification tool would fail to minify ES6 JavaScript and break the build."
     },
     {
       name: "mail-for-good",
-      url: "https://github.com/seanWLawrence/mail-for-good",
+      url: "https://github.com/freeCodeCamp/mail-for-good/pull/251",
       programmingLanguage: "HTML/CSS",
-      description: "Create accessible documentation website."
+      description:
+        "Created an accessible documentation website for Mail For Good, an open source email campaign management tool for nonprofits. The site is deployed on GitHub Pages and is made with Hugo and a customized Material Design theme with Free Code Camp's branding."
     },
     {
       name: "MINA",
       url: "http://mina.pro",
       programmingLanguage: "HTML/CSS",
       description:
-        "Create a basic Wordpress website for a non-profit organization."
+        "Created a website for the Mobile Innovation Network and Association non-profit company that and allows filmmakers to submit films for screening events hosted by them and view information about past and upcoming screenings. The site was built with WordPress and integrates with third-party tools from FilmFreeway and Scribd."
     }
   ],
   openSourceProjects: [
@@ -27,21 +28,21 @@ export default {
       url: "https://github.com/seanWLawrence/hendrix",
       programmingLanguage: "TypeScript",
       description:
-        "🔧 Simple, Rails-like CLI tool for generating files quickly."
+        "Command line interface that allows you to quickly generate list of files based on customizable templates with zero configuration. We use this in production at Make School to scaffold new products and onboard new engineers with our frontend coding conventions."
     },
     {
       name: "curry-time (in progress)",
       url: "https://github.com/seanWLawrence/curry-time",
       programmingLanguage: "TypeScript",
       description:
-        "🍜 100% curried functional programming helpers to help you write more declarative logic."
+        "Curried functional programming helpers to help you write chainable, declarative logic with immutable data. Includes over 60 functions like pipe, caseOf, maybe, pluck, etc., detailed TypeScript type definitions, and thorough property-based testing."
     },
     {
       name: "react-design-system (in progress)",
       url: "https://github.com/seanWLawrence/react-design-system",
       programmingLanguage: "TypeScript/SASS",
       description:
-        "🚧 Accessible, easily customizable components for building a design system in React."
+        "Command line interface that allows you to generate a list of basic and advanced React components with base styles and built-in accessibility, form validation, TypeScript type definitions and error handling without any dependencies. The base styles build off of a SASS theme that can easily be customized for your project's branding."
     }
   ]
 };
--- src/data/publicSpeaking.ts
+++ src/data/publicSpeaking.ts
@@ -3,27 +3,28 @@ export default {
     {
       name: "Don't block the frontend!",
       description:
-        "Optimizing product development workflows using GraphQL contracts.",
+        'The concept is that before development starts on a new product, the frontend and backend teams agree on a GraphQL API, create the API using mocked data, and then both teams work simultanesouly on the new product until an MVP is ready. Lastly, both teams push their code and the feeature should "just work."',
       location: "GraphQL Montreal",
       url: "https://bit.ly/dont-block-the-frontend"
     },
     {
       name: "An introduction to Cypress",
       description:
-        "How to write automated integration tests for accessibility and functionality.",
+        "This covered how to use Make School's custom Cypress commands to interact with Rails, like logging in programmatically, creating models, and executing Ruby files in the Rails context. We then wrote an integration test for our registering for courses together to put these commands to use in a real-world scenario.",
       url:
         "https://makeschool.github.io/an-introduction-to-cypress/?slide=0&slideElement=-1",
       location: "Make School"
     },
     {
       name: "Testing React with Enzyme and Jest",
-      description: "Best practices for testing React components.",
+      description:
+        "This covered best practices for testing React components, such as interacting with the page as a user would, avoid testing implementation details like state, and using stable selectors like test ids. Note: Nowadays it's better to use a framework like @testing-library/react and select components by text, label text, etc. instead of test ids.",
       location: "Make School"
     },
     {
       name: "Writing GraphQL queries and mutations with Apollo",
       description:
-        "Best practices for querying and mutating data using react-apollo.",
+        "This covered how to switch the mental model from using our legacy GraphQL client Relay Classic to the new Apollo Client for React. We discussed syntax changes, how to update the Apollo cache after a mutation, and how to use a custom higher order component and error handlers that we created to handle loading states and server or mutation errors.",
       location: "Make School"
     }
   ]
